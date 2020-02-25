2369f5596eb5f7e2f5af53bf63feff710e7d5a97
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-02-25 22:48:57 +0100

    Version OK

---
+++ CloudPicker/CloudPicker.swift
@@ -0,0 +1,170 @@
+//
+//  CloudPicker.swift
+//  CloudPicker
+//
+//  Created by Slawek Kurczewski on 25/02/2020.
+//  Copyright © 2020 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+protocol CloudPickerDeleate {
+    func documentsPicked(documents: [CloudPicker.Document]?)
+}
+class CloudPicker: NSObject {
+    var delegate: CloudPickerDeleate?
+    private var pickerController: UIDocumentPickerViewController?
+    private weak var presentationController: UIViewController?
+    private var folderUrl: URL?
+    private var typeOfSource: TypeOfSource!
+    private var documents = [Document]()
+    
+    enum TypeOfSource: Int {
+        case folders
+        case files
+    }
+    class Document: UIDocument {
+        var data: Data?
+        override func contents(forType typeName: String) throws -> Any {
+            guard let data = data else {  return Data()   }
+            return try NSKeyedArchiver.archivedData(withRootObject: data, requiringSecureCoding: true)
+        }
+        override func load(fromContents contents: Any, ofType typeName: String?) throws {
+            guard let data = contents as? Data else {   return   }
+            self.data = data
+        }
+    }
+
+    init(presentationController: UIViewController) {
+        super.init()
+        self.presentationController = presentationController
+    }
+    public func folderAction(for type: TypeOfSource, title: String) -> UIAlertAction? {
+        let action = UIAlertAction(title: title, style: .default) { (alertAction) in
+            self.pickerController = UIDocumentPickerViewController(documentTypes: [kUTTypeFolder as String], in:    .open)
+            self.pickerController?.delegate = self
+            self.typeOfSource = type
+            self.presentationController?.present(self.pickerController!, animated: true)
+        }
+        return action
+    }
+    public func fileAction(for type: TypeOfSource, title: String) -> UIAlertAction? {
+        let action = UIAlertAction(title: title, style: .default) { (alertAction) in
+            //let keyArray = [kUTTypePDF, kUTTypeText,kUTTypeBMP, kUTTypePNG, kUTTypeArchive, kUTTypeZipArchive] as [String]
+            let keyArray = [kUTTypeMovie as String, kUTTypeImage as String]
+            self.pickerController = UIDocumentPickerViewController(documentTypes: keyArray, in:    .open)
+            self.pickerController?.delegate = self
+            self.pickerController?.allowsMultipleSelection = true
+            self.typeOfSource = type
+            self.presentationController?.present(self.pickerController!, animated: true)
+        }
+        return action
+    }
+    public func present(from soureView: UIView) {
+        let allertController = UIAlertController(title: "Select from", message: nil, preferredStyle: .actionSheet)
+        if let action = self.folderAction(for: .files, title: "Files") {
+            allertController.addAction(action)
+        }
+        if let action = self.folderAction(for: .folders, title: "Folder") {
+            allertController.addAction(action)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        allertController.addAction(cancel)
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            allertController.popoverPresentationController?.sourceView = soureView
+            allertController.popoverPresentationController?.sourceRect = soureView.bounds
+            allertController.popoverPresentationController?.permittedArrowDirections = [ .down, .up]
+        }
+        self.presentationController?.present(allertController, animated: true)
+    }
+}
+extension CloudPicker: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else {  return  }
+        print("urls:\(urls), count:\(urls.count)")
+        documentFromURL(pickedURL: url)
+        delegate?.documentsPicked(documents: documents)
+        print("documents count:\(documents.count)")
+    }
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("Cancel picker")
+        delegate?.documentsPicked(documents: nil)
+    }
+    private func documentFromURL(pickedURL: URL) {
+        let shouldStopAccessing = pickedURL.startAccessingSecurityScopedResource()
+        defer {
+            if shouldStopAccessing {
+                pickedURL.stopAccessingSecurityScopedResource()
+            }
+        }
+        
+        NSFileCoordinator().coordinate(readingItemAt: pickedURL, error: NSErrorPointer.none) { (folderURL) in
+
+                        do {
+                            let keys: [URLResourceKey] = [.nameKey, .isDirectoryKey]
+                            let fileList = try FileManager.default.enumerator(at: pickedURL, includingPropertiesForKeys: keys)
+
+                            switch typeOfSource {
+                            case .files:
+                                let document = Document(fileURL: pickedURL)
+                                documents.append(document)
+                            
+                            case .folders:
+                                for case let fileURL as URL in fileList! {
+                                    if !isUrlDirectory(fileURL) {
+                                        let document = Document(fileURL: fileURL)
+                                        documents.append(document)
+                                    }
+                                }
+                            case .none:
+                                break
+                            }
+
+                        } catch let error {
+                            print("error: ", error.localizedDescription)
+                        }
+
+                    }
+    }
+//   private func documentFromURL(pickedURL: URL) {
+//          let shouldStopAccessing = pickedURL.startAccessingSecurityScopedResource()
+//          defer {
+//              if shouldStopAccessing {
+//                  pickedURL.stopAccessingSecurityScopedResource()
+//              }
+//          }
+//          NSFileCoordinator().coordinate(readingItemAt: pickedURL, error: NSErrorPointer.none) { (folderURL) in
+//                  do {
+//                      let keys: [URLResourceKey] = [.nameKey, .isDirectoryKey]
+//                      let fileList = FileManager.default.enumerator(at: pickedURL, includingPropertiesForKeys: keys)
+//
+//                      switch typeOfSource {
+//                      case .files:
+//                          let document = Document(fileURL: pickedURL)
+//                          documents.append(document)
+//
+//                      case .folders:
+//                          for case let fileURL as URL in fileList! {
+//                              if !isUrlDirectory(fileURL) {
+//                                  let document = Document(fileURL: fileURL)
+//                                  documents.append(document)
+//                              }
+//                          }
+//                      case .none:
+//                          break
+//                    }
+//                  }
+//
+//            }
+    func isUrlDirectory(_ url: URL) -> Bool {
+        let resUrl = try? url.resourceValues(forKeys: [.isDirectoryKey]).isDirectory
+        if let myUrl = resUrl {
+            return myUrl
+        }
+        else {
+            return false
+        }
+    }
+}
--- CloudPicker/ViewController.swift
+++ CloudPicker/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CloudPickerDeleate {
+    var documents = [CloudPicker.Document]()
+    var cloudPicker: CloudPicker!
+    private let refreshControl = UIRefreshControl()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func documentsPicked(documents: [CloudPicker.Document]?) {
+        print("documentsPicked:\(String(describing: documents))")
+        documents?.forEach({ (elem) in
+            self.documents.append(elem)
+        })
+        print("XXX:\(String(describing: documents))")
+    }
+    @IBAction func addFiles(_ sender: UIBarButtonItem) {
+        cloudPicker.present(from: view)
+        tableView.reloadData()
+    }
+    @IBAction func refreshAction(_ sender: UIBarButtonItem) {
+          tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        documents = []
+        tableView.delegate = self
+        cloudPicker = CloudPicker(presentationController: self)
+        cloudPicker.delegate = self
+        // Add Refresh Control to Table View
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refreshControl
+        } else {
+            tableView.addSubview(refreshControl)
+        }
+        // Configure Refresh Control
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+    
+        refreshControl .attributedTitle = NSAttributedString(string: "Proszę czekać...", attributes: .none)
     }
+    @objc private func refreshData(_ sender: Any) {
+        // Fetch Weather Data
+        print("Refresh Data")
+        tableView.reloadData()
+        //tableView.updateView()
+        self.refreshControl.endRefreshing()
+        //self.activityIndicatorView.stopAnimating()
 
-
+    }
+}
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return documents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row):"
+        cell.detailTextLabel?.text = "\(documents[indexPath.row].fileURL)"
+        return cell
+    }
 }
 
