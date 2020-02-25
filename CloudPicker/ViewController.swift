bf8834d1c9d63f9ca2caa992550c099df88aa5f8
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-02-25 09:45:46 +0100

    Initial Commit

---
+++ CloudPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudPicker
+//
+//  Created by Slawek Kurczewski on 25/02/2020.
+//  Copyright © 2020 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CloudPickerTests/CloudPickerTests.swift
@@ -0,0 +1,34 @@
+//
+//  CloudPickerTests.swift
+//  CloudPickerTests
+//
+//  Created by Slawek Kurczewski on 25/02/2020.
+//  Copyright © 2020 Slawomir Kurczewski. All rights reserved.
+//
+
+import XCTest
+@testable import CloudPicker
+
+class CloudPickerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
